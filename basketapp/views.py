6dd473bbe7d487d68902ef3d5602fe0e1c27659d
mrwindmark <agentstorm@ya.ru>
2021-03-11 00:57:56 +0300

    basket view updated

--- basketapp/views.py
+++ basketapp/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponseRedirect
+from django.urls import reverse
+from mainapp.models import Product
+from basketapp.models import Basket
+
 
 # Create your views here.
+def basket_add(request, prod_id):
+    curr_product = Product.objects.get(id=prod_id)
+    curr_basket = Basket.objects.filter(user=request.user, product=curr_product)
+
+    if not curr_basket.exists():
+        basket = Basket(user=request.user, product=curr_product)
+        basket.quantity = 1
+        basket.save()
+        return HttpResponseRedirect(reverse('auth:profile'))
+    else:
+        basket = curr_basket.first()
+        basket.quantity += 1
+        basket.save()
+        return HttpResponseRedirect(reverse('auth:profile'))
