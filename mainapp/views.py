21386f19ee0cfbbe14e9c1430abb2e1325c0f932
mrwindmark <agentstorm@ya.ru>
2021-04-22 11:43:52 +0300

    low level cache in use now

--- mainapp/views.py
+++ mainapp/views.py
@@ -17,12 +17,15 @@ def index(request):
 def products(request, category_id=None, page_number=1):
     context = {
         'title': 'GeekShop - Каталог',
-        'categories': ProductCategory.objects.all(),
+        # 'categories': ProductCategory.objects.all(),
+        'categories': get_links_menu(),
     }
     if category_id:
-        products = Product.objects.filter(category_id=category_id).select_related('category')
+        # products = Product.objects.filter(category_id=category_id).select_related('category')
+        products = get_products_in_category(category_id)
     else:
-        products = Product.objects.all().select_related('category')
+        # products = Product.objects.all().select_related('category')
+        products = get_all_products()
     paginator = Paginator(products, per_page=3)
     products_paginator = paginator.page(page_number)
     context.update({'products': products_paginator})
@@ -34,35 +37,23 @@ def get_links_menu():
         key = 'links_menu'
         links_menu = cache.get(key)
         if links_menu is None:
-            links_menu = ProductCategory.objects.filter(is_active=True)
+            links_menu = ProductCategory.objects.all()
             cache.set(key, links_menu)
         return links_menu
     else:
-        return ProductCategory.objects.filter(is_active=True)
+        return ProductCategory.objects.all()
 
 
-def get_category(pk):
-    if settings.LOW_CACHE:
-        key = f'category_{pk}'
-        category = cache.get(key)
-        if category is None:
-            category = get_object_or_404(ProductCategory, pk=pk)
-            cache.set(key, category)
-        return category
-    else:
-        return get_object_or_404(ProductCategory, pk=pk)
-
-
-def get_products():
+def get_all_products():
     if settings.LOW_CACHE:
         key = 'products'
         products = cache.get(key)
         if products is None:
-            products = Product.objects.filter(is_active=True, category__is_active=True).select_related('category')
+            products = Product.objects.all().select_related('category')
             cache.set(key, products)
         return products
     else:
-        return Product.objects.filter(is_active=True, category__is_active=True).select_related('category')
+        return Product.objects.all().select_related('category')
 
 
 def get_product(pk):
@@ -77,27 +68,13 @@ def get_product(pk):
         return get_object_or_404(Product, pk=pk)
 
 
-def get_products_orederd_by_price():
+def get_products_in_category(pk):
     if settings.LOW_CACHE:
-        key = 'products_orederd_by_price'
+        key = f'products_in_category_{pk}'
         products = cache.get(key)
         if products is None:
-            products = Product.objects.filter(is_active=True, category__is_active=True).order_by('price')
+            products = Product.objects.filter(category__pk=pk)
             cache.set(key, products)
         return products
     else:
-        return Product.objects.filter(is_active=True, category__is_active=True).order_by('price')
-
-
-def get_products_in_category_orederd_by_price(pk):
-    if settings.LOW_CACHE:
-        key = f'products_in_category_orederd_by_price_{pk}'
-        products = cache.get(key)
-        if products is None:
-            products = Product.objects.filter(
-                category__pk=pk, is_active=True,
-                category__is_active=True).order_by('price')
-            cache.set(key, products)
-        return products
-    else:
-        return Product.objects.filter(category__pk=pk, is_active=True, category__is_active=True).order_by('price')
+        return Product.objects.filter(category__pk=pk)
