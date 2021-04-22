d4e481b2147966101bb1939566788a4d1f277ce4
mrwindmark <agentstorm@ya.ru>
2021-04-22 13:44:00 +0300

    static files issue. I can't solve it in local PC

--- mercury/settings.py
+++ mercury/settings.py
@@ -188,9 +188,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+# Static_root didn't work anymore?
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static/'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 MEDIA_URL = '/media/'
